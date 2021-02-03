7862afe749e96ade3c7c5667f4f84f568368249e
Fabien HIEGEL <fabien.hiegel@gmail.com>
2021-02-03 10:22:54 +0100

    fix: setup prod baseUrl

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      base: 'https://de-dale.github.io/gargoyles/'
+    })
+  ]
   // optimization: {
   //   splitChunks: {
   //     cacheGroups: {
