835b465b1dcff97bf3d749e25a517774947f7d67
Fabien HIEGEL <fabien.hiegel@gmail.com>
2021-02-05 14:44:23 +0100

    chore: add eslint and editorconfig

--- src/app/levels/sandbox.ts
+++ src/app/levels/sandbox.ts
@@ -1,52 +1,50 @@
-import * as Phaser from 'phaser';
+import * as Phaser from 'phaser'
 
-import * as tiles from '@assets/maps/colored.png';
-import * as levelSandbox from '@assets/maps/level-sandbox.json';
+import * as tiles from '@assets/maps/colored.png'
+import * as levelSandbox from '@assets/maps/level-sandbox.json'
 
-import { FirebrandOnAction } from '@app/player/firebrand/onAction';
+import { FirebrandOnAction } from '@app/player/firebrand/onAction'
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
-  key: 'Gargoyles',
-};
+  key: 'Gargoyles'
+}
 
 export class SandboxScene extends Phaser.Scene {
-
   private firebrand: FirebrandOnAction;
 
-  constructor() {
-    super(sceneConfig);
+  constructor () {
+    super(sceneConfig)
   }
 
-  public preload() {
-    this.load.image('tiles', tiles);
-    this.load.tilemapTiledJSON('levelSandbox', levelSandbox);
+  public preload () {
+    this.load.image('tiles', tiles)
+    this.load.tilemapTiledJSON('levelSandbox', levelSandbox)
 
-    FirebrandOnAction.preloadSprites(this);
+    FirebrandOnAction.preloadSprites(this)
   }
 
-  public create() {
-    const map = this.make.tilemap({ key: "levelSandbox" })
+  public create () {
+    const map = this.make.tilemap({ key: 'levelSandbox' })
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
-    const tileset = map.addTilesetImage("colored", "tiles")
-    const worldLayer = map.createLayer("world", tileset)
+    const tileset = map.addTilesetImage('colored', 'tiles')
+    const worldLayer = map.createLayer('world', tileset)
     worldLayer.setCollisionByProperty({ solid: true })
 
     this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-    this.firebrand = this.createFirebrand(map);
+    this.firebrand = this.createFirebrand(map)
 
-    this.physics.add.collider(this.firebrand.avatar, worldLayer);
+    this.physics.add.collider(this.firebrand.avatar, worldLayer)
   }
 
-  private createFirebrand(map: Phaser.Tilemaps.Tilemap) {
-    const { x: startX, y: startY } = map.findObject("objects", i => i.name === "start-position");
-    return FirebrandOnAction.create(this, startX, startY);
+  private createFirebrand (map: Phaser.Tilemaps.Tilemap) {
+    const { x: startX, y: startY } = map.findObject('objects', i => i.name === 'start-position')
+    return FirebrandOnAction.create(this, startX, startY)
   }
 
-  public update(time: number, delta: number) {
+  public update (time: number, delta: number) {
     this.firebrand.update()
   }
-
 }
--- src/app/player/firebrand/onAction.ts
+++ src/app/player/firebrand/onAction.ts
@@ -1,7 +1,7 @@
-import * as Phaser from "phaser";
+import * as Phaser from 'phaser'
 
-import * as firebrand from '@assets/sprites/firebrand.png';
-import * as firebrandAtlas from '@assets/sprites/firebrand.atlas.json';
+import * as firebrand from '@assets/sprites/firebrand.png'
+import * as firebrandAtlas from '@assets/sprites/firebrand.atlas.json'
 
 export class FirebrandOnAction {
   avatar: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
@@ -11,7 +11,7 @@ export class FirebrandOnAction {
   right: Phaser.Input.Keyboard.Key
   jumping: boolean = false;
 
-  constructor(player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, {
+  constructor (player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, {
     up,
     down,
     left,
@@ -24,14 +24,14 @@ export class FirebrandOnAction {
     this.right = right
   }
 
-  static preloadSprites(scene: Phaser.Scene) {
+  static preloadSprites (scene: Phaser.Scene) {
     scene.load.atlas('firebrand', firebrand, firebrandAtlas)
   }
 
-  static create(scene: Phaser.Scene, x, y){
-    const cursors = scene.input.keyboard.createCursorKeys();
-    const player = scene.physics.add.sprite(x, y, 'firebrand', 'firebrand.stand');
-    player.setCollideWorldBounds(true);
+  static create (scene: Phaser.Scene, x, y) {
+    const cursors = scene.input.keyboard.createCursorKeys()
+    const player = scene.physics.add.sprite(x, y, 'firebrand', 'firebrand.stand')
+    player.setCollideWorldBounds(true)
     scene.cameras.main.startFollow(player)
 
     const result = new FirebrandOnAction(player, cursors)
@@ -39,134 +39,140 @@ export class FirebrandOnAction {
     return result
   }
 
-  private createAnimations(scene: Phaser.Scene) {
+  private createAnimations (scene: Phaser.Scene) {
     this.avatar.anims.create({
       key: 'walk',
       frames: scene.anims.generateFrameNames('firebrand', { prefix: 'firebrand.walk', start: 1, end: 3 }),
       frameRate: 10,
       repeat: -1
-    });
+    })
 
     this.avatar.anims.create({
       key: 'jump',
       frames: scene.anims.generateFrameNames('firebrand', { prefix: 'firebrand.jump', start: 1, end: 3 }),
       frameRate: 10,
       repeat: -1
-    });
+    })
   }
 
-  private isOnFloor(): boolean {
+  private isOnFloor (): boolean {
     return this.avatar.body.onFloor()
   }
 
-  private leftIsDown(): boolean {
-    return this.left.isDown;
+  private leftIsDown (): boolean {
+    return this.left.isDown
   }
 
-  private leftIsUp(): boolean {
-    return this.left.isUp;
+  private leftIsUp (): boolean {
+    return this.left.isUp
   }
 
-  private rightIsDown(): boolean {
-    return this.right.isDown;
+  private rightIsDown (): boolean {
+    return this.right.isDown
   }
 
-  private rightIsUp(): boolean {
-    return this.right.isUp;
+  private rightIsUp (): boolean {
+    return this.right.isUp
   }
 
-  private upIsDown(): boolean {
-    return this.up.isDown;
+  private upIsDown (): boolean {
+    return this.up.isDown
   }
 
-  private upIsUp(): boolean {
-    return this.up.isUp;
+  private upIsUp (): boolean {
+    return this.up.isUp
   }
 
-  private movesRight() {
-    return this.leftIsUp() && this.rightIsDown();
+  private movesRight () {
+    return this.leftIsUp() && this.rightIsDown()
   }
 
-  private movesLeft() {
-    return this.leftIsDown() && this.rightIsUp();
+  private movesLeft () {
+    return this.leftIsDown() && this.rightIsUp()
   }
 
-  private cannotDetermineMove() {
-    return this.leftIsDown() && this.rightIsDown() || this.leftIsUp() && this.rightIsUp();
+  private bothDirectionsAreDown () {
+    return this.leftIsDown() && this.rightIsDown()
   }
 
-  private animateOrientation() {
+  private bothDirectionsAreUp () {
+    return this.leftIsUp() && this.rightIsUp()
+  }
+
+  private cannotDetermineMove () {
+    return this.bothDirectionsAreDown() || this.bothDirectionsAreUp()
+  }
+
+  private animateOrientation () {
     if (this.movesLeft()) {
-      this.avatar.setFlip(true, false);
+      this.avatar.setFlip(true, false)
     }
     if (this.movesRight()) {
-      this.avatar.setFlip(false, false);
+      this.avatar.setFlip(false, false)
     }
   };
 
   private moveOnHorizontally = () => {
     if (this.movesLeft()) {
-      this.avatar.setVelocityX(-160);
+      this.avatar.setVelocityX(-160)
     }
     if (this.movesRight()) {
-      this.avatar.setVelocityX(160);
+      this.avatar.setVelocityX(160)
     }
     if (this.cannotDetermineMove()) {
-      this.avatar.setVelocityX(0);
+      this.avatar.setVelocityX(0)
     }
   };
 
-  private animateWalking() {
+  private animateWalking () {
     if (!this.isOnFloor()) {
-      return;
+      return
     }
     if (this.movesLeft() || this.movesRight()) {
-      this.avatar.anims.play('walk', true);
+      this.avatar.anims.play('walk', true)
     } else {
-      this.avatar.setTexture("firebrand", "firebrand.stand")
+      this.avatar.setTexture('firebrand', 'firebrand.stand')
     }
   }
 
-  private startsJumping() {
-    return this.isOnFloor() && this.upIsDown();
+  private startsJumping () {
+    return this.isOnFloor() && this.upIsDown()
   }
 
-  private isJumping() {
-    return this.jumping;
+  private isJumping () {
+    return this.jumping
   }
 
-  private jumps() {
+  private jumps () {
     if (this.startsJumping()) {
-      this.jumping = true;
-      this.avatar.setVelocityY(-400);
+      this.jumping = true
+      this.avatar.setVelocityY(-400)
     }
     if (this.isJumping() && this.upIsUp()) {
-      this.jumping = false;
-      this.avatar.setVelocityY(0);
+      this.jumping = false
+      this.avatar.setVelocityY(0)
     }
   }
 
-  private animateJumping() {
+  private animateJumping () {
     if (!this.isOnFloor()) {
-      this.avatar.anims.play('jump', true);
+      this.avatar.anims.play('jump', true)
     }
   }
 
-
-  private flying() {
+  private flying () {
     if (!this.isOnFloor() && !this.isJumping() && this.upIsDown()) {
-      this.avatar.setVelocityY(-5);
+      this.avatar.setVelocityY(-5)
     }
   };
 
-  update() {
-    this.moveOnHorizontally();
-    this.animateOrientation();
-    this.animateWalking();
+  update () {
+    this.moveOnHorizontally()
+    this.animateOrientation()
+    this.animateWalking()
 
-    this.jumps();
-    this.animateJumping();
+    this.jumps()
+    this.animateJumping()
     this.flying()
   }
-
 }
